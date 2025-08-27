f6b3fa04b4f43bfe79d6479c62e4b8f0771db028
Jose Mateo Lopez Cifuentes <aprendiz.it@consulting.local>
2025-08-27 15:41:02 -0500

    cambios

--- frontend/src/pages/PerformanceEvaluation.js
+++ frontend/src/pages/PerformanceEvaluation.js
@@ -1975,7 +1975,7 @@ function PerformanceEvaluation() {
               </tr>
               <tr>
                 <td style={{ backgroundColor: "#fff", padding: "0.5rem 0.4rem" }}>
-                  {rows[21].aspecto}
+                  {rows[21].aspecto}u
                 </td>
                 <td style={{ backgroundColor: "#fff", padding: "0.5rem 0.4rem" }}>
                     <select
